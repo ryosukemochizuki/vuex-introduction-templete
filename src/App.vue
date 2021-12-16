5c531ae5e360b2c51ce869d254850ec67d8cd562
ryosukemochizuki <ryosukemochizuki.mochi@gmail.com>
2021-12-16 22:55:37 +0900

    :art: vuexの練習問題を作成

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,28 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Welcome to Your Vue.js App" />
+  <p>count: {{ count }}</p>
+  <p>double-count: {{ double }}</p>
+  <button @click="countUp">押す</button>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import { useStore } from "vuex";
+import { computed } from "@vue/reactivity";
 
 export default {
   name: "App",
   components: {
     HelloWorld,
+  },
+  setup() {
+    const store = useStore();
+    return {
+      count: computed(() => store.state.count),
+      double: computed(() => store.getters.double),
+      countUp: () => store.dispatch("sayHi"),
+    };
   },
 };
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,27 @@
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  state() {
+    return {
+      count: 0,
+      message: "hello, world",
+    };
+  },
+  getters: {
+    double(state) {
+      return state.count * 2;
+    },
+  },
+  mutations: {
+    plusOne(state) {
+      state.count++;
+    },
+  },
+  actions: {
+    sayHi({ commit }) {
+      setTimeout(() => {
+        commit("plusOne");
+      }, 1000);
+    },
+  },
 });
